acff1e2298bc195562eb454fe1974c6d8eb241fa
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-04-28 11:56:51 +0200

    setTotalIncome works

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ function App() {
   const [income, setIncome] = useState([]);
   const [totalIncome, setTotalIncome] = useState(0);
 
+  useEffect(() => {
+    let temp = 0;
+    for (let i = 0; i < income.length; i++) {
+      temp += parseInt(income[i].price);
+    }
+
+    setTotalIncome(temp);
+  }, [income]);
+
   return (
     <div className="App">
       <Header totalIncome={totalIncome} />
       <IncomeForm income={income} setIncome={setIncome} />
-      {income.length > 0 ? income[0].desc : ""}
     </div>
   );
 }
