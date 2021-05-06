e311596c49661be7987ae081a9d53ebfeaa66c5e
Clemfrl <63780455+Clemfrl@users.noreply.github.com>
2021-05-06 10:45:24 +0200

    Import Income List to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import IncomeForm from "./components/IncomeForm";
+import IncomeList from "./components/IncomeList";
 
 function App() {
   const [income, setIncome] = useState([]);
@@ -19,6 +20,7 @@ function App() {
     <div className="App">
       <Header totalIncome={totalIncome} />
       <IncomeForm income={income} setIncome={setIncome} />
+      <IncomeList income={income} setIncome={setIncome} />
     </div>
   );
 }
